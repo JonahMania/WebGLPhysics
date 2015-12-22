76831bd3e7631b8ac80b26b95ca207f7bb5037af
jonahmania <jmania999@gmail.com>
2015-12-21 20:29:38 -0500

    Add sphere sphere collision resolution

--- src/app/createObjects.js
+++ src/app/createObjects.js
@@ -8,7 +8,7 @@ define(['app/factory'],function(factory)
         objects = [];
         //Sphere
         objects.push(
-            fact.createSphere( -3.0, -1.9, 2, 1, 20, 20, 1.0, 0.0, 0.0, 0.2, true )
+            fact.createSphere( -3.0, 4, 1, 1, 20, 20, 1.0, 0.0, 0.0, 0.2, true )
         );
         //Ground plate
         objects.push(
@@ -18,14 +18,22 @@ define(['app/factory'],function(factory)
         objects.push(
             fact.createRect( 0, -2.4, 0, 7, 7, 4, 0.3, 0.2, 0.8, 1.0, false )
         );
-        //Sphere 2
+        //Sphere
         objects.push(
-            fact.createSphere( 3.0, -1.9, 3, 1, 20, 20, 1.0, 0.0, 0.0, 0.2, true )
+            fact.createSphere( -3, 3, 3, 1, 20, 20, 1.0, 0.0, 0.0, 0.2, true )
         );
-        // // Sphere 3
-        // objects.push(
-        //     fact.createSphere( 5.0, 2.0, -2, 1, 20, 20, 1.0, 0.0, 0.0, 0.2, true )
-        // );
+        // Sphere
+        objects.push(
+            fact.createSphere( -3, 5, 5, 1, 20, 20, 1.0, 0.0, 0.0, 0.2, true )
+        );
+        // Sphere
+        objects.push(
+            fact.createSphere( 3.0, 3, 3, 1, 20, 20, 1.0, 0.0, 0.0, 0.2, true )
+        );
+        // Sphere
+        objects.push(
+            fact.createSphere( 3.0, 2, 1, 1, 20, 20, 1.0, 0.0, 0.0, 0.2, true )
+        );
 
     }
     var setPosition = function( gl )
@@ -38,12 +46,17 @@ define(['app/factory'],function(factory)
 
         //Sphere 0
         objects[0].velocity[0] = 4;
-        objects[0].velocity[2] = 2;
+        objects[0].velocity[2] = -4;
         //Ground plate 1
         //Back plate 2
         //Sphere 3
-        objects[3].velocity[0] = -4;
-        // objects[3].velocity[2] = 2;
+        objects[3].velocity[0] = 8;
+        //Sphere 4
+        objects[4].velocity[0] = 2;
+        objects[4].velocity[2] = 2;
+        //Sphere 5
+        objects[5].velocity[0] = -1;
+        //Sphere 6
     }
     var getObjects = function()
     {
--- src/app/physics/collisionResolution/resolveSphereSphere.js
+++ src/app/physics/collisionResolution/resolveSphereSphere.js
@@ -2,8 +2,27 @@ define(['gl-matrix-min'],function(glm)
 {
     return function( gl, sphereA, sphereB, distance )
     {
-        //Scale the distance of the spheres so they move out of collision
+        //Scale the direction of both spheres so they avoid collision
         glm.vec3.scale( sphereA.direction, sphereA.direction, distance );
         glm.vec3.scale( sphereB.direction, sphereB.direction, distance );
+
+        // vAf = ( vAi * ( mA - mB ) + 2 * mB * vBi ) / ( mA + mB )
+        var velA = glm.vec3.create();
+        var tempA = glm.vec3.create();
+        glm.vec3.scale( velA, sphereA.velocity, ( sphereA.mass - sphereB.mass ) );
+        glm.vec3.scale( tempA, sphereB.velocity, ( 2 * sphereB.mass ) );
+        glm.vec3.add( velA, velA, tempA );
+        glm.vec3.scale( velA, velA, 1/( sphereA.mass + sphereB.mass ) );
+
+        // vBf = ( vBi * ( mB - mA ) + 2 * mA * vAi ) / ( mA + mB )
+        var velB = glm.vec3.create();
+        var tempB = glm.vec3.create();
+        glm.vec3.scale( velB, sphereB.velocity, ( sphereB.mass - sphereA.mass ) );
+        glm.vec3.scale( tempB, sphereA.velocity, ( 2 * sphereA.mass ) );
+        glm.vec3.add( velB, velB, tempB );
+        glm.vec3.scale( velB, velB, 1/( sphereA.mass + sphereB.mass ) );
+
+        sphereA.velocity = velA;
+        sphereB.velocity = velB;
     }
 });
